816b0c86284486e2baa98ee422b39eaf26535a69
Philip Hofstetter <phofstetter@sensational.ch>
2025-01-08 15:49:55 +0100

    run swift-format as a github action

--- Daylight Watch App/SolarCalculator.swift
+++ Daylight Watch App/SolarCalculator.swift
@@ -11,12 +11,12 @@ struct SolarCalculator {
     self.date = date
 
     if date < sun.decemberSolstice && date < sun.juneSolstice {
-      let last_year = Calendar.current.date(byAdding: DateComponents(year: -1), to: date)!
+      let lastYear = Calendar.current.date(byAdding: DateComponents(year: -1), to: date)!
 
-      let last_sun = Sun(location: location, timeZone: Calendar.current.timeZone, date: last_year)
+      let lastSun = Sun(location: location, timeZone: Calendar.current.timeZone, date: lastYear)
 
       sunAtSolstice = Sun(
-        location: location, timeZone: Calendar.current.timeZone, date: last_sun.decemberSolstice)
+        location: location, timeZone: Calendar.current.timeZone, date: lastSun.decemberSolstice)
     } else if date < sun.decemberSolstice {
       sunAtSolstice = Sun(
         location: location, timeZone: Calendar.current.timeZone, date: sun.juneSolstice)
@@ -57,20 +57,20 @@ struct SolarCalculator {
     let now = self.sun.civilDawn
     let then = self.sunAtSolstice.civilDawn
 
-    let seconds_now = now.secondsSinceMidnight
-    let seconds_then = then.secondsSinceMidnight
+    let secondsNow = now.secondsSinceMidnight
+    let secondsThen = then.secondsSinceMidnight
 
-    return Double(seconds_then - seconds_now)
+    return Double(secondsThen - secondsNow)
   }
 
   public var eveningTimeSinceSolistice: TimeInterval {
     let now = self.sun.civilDusk
     let then = self.sunAtSolstice.civilDusk
 
-    let seconds_now = now.secondsSinceMidnight
-    let seconds_then = then.secondsSinceMidnight
+    let secondsNow = now.secondsSinceMidnight
+    let secondsThen = then.secondsSinceMidnight
 
-    return Double(seconds_now - seconds_then)
+    return Double(secondsNow - secondsThen)
   }
 
 }
