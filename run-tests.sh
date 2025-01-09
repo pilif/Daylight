c9c6f1261e0e089ceb66e4f495e93adfe9e9517b
Philip Hofstetter <phofstetter@sensational.ch>
2025-01-09 15:10:18 +0100

    try harder to find the correct destination
    *sigh*

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 set -euo pipefail
-id=$(xcodebuild test -showdestinations -scheme "Daylight Watch App" -target "Daylight Watch AppTests" | grep "watchOS Simulator" | grep "Apple Watch Series 10 (46mm)" | head -n 1 | grep -Eo 'id:.*?,' | sed  's/id://' | sed 's/,$//')
-echo "using destination id $id"
-xcodebuild test -scheme "Daylight Watch App" -target "Daylight Watch AppTests" -destination "id=$id" CODE_SIGNING_ALLOWED='NO'
+id="$(xcodebuild test -showdestinations -scheme "Daylight Watch App" -target "Daylight Watch AppTests" | grep "watchOS Simulator" | grep "Apple Watch Series 10 (46mm)" | head -n 1 | grep -Eo 'id:.*?,' | sed  's/id://' | sed 's/,$//')"
+xcodebuild test -scheme "Daylight Watch App" -target "Daylight Watch AppTests" -destination "platform=watchOS Simulator,id=$id" CODE_SIGNING_ALLOWED='NO'
