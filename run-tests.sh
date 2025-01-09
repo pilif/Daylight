12cebb453b798458db4da21d442848e28f1ea01d
Philip Hofstetter <phofstetter@sensational.ch>
2025-01-09 15:56:22 +0100

    attach test results to runs on github actions

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
 set -euo pipefail
+
+if [ -d result ]; then
+    rm -r result
+fi
+
+mkdir -p result
+
 id="$(xcodebuild test -showdestinations -scheme "Daylight Watch App" -target "Daylight Watch AppTests" | grep "watchOS Simulator" | grep "Apple Watch Series 10 (46mm)" | head -n 1 | grep -Eo 'id:.*?,' | sed  's/id://' | sed 's/,$//')"
-xcodebuild test -scheme "Daylight Watch App" -target "Daylight Watch AppTests" -destination "platform=watchOS Simulator,id=$id" CODE_SIGNING_ALLOWED='NO'
+xcodebuild test \
+    -scheme "Daylight Watch App" \
+    -target "Daylight Watch AppTests" \
+    -destination id="$id" \
+    -resultBundlePath result/test-results.xcresult \
+    CODE_SIGNING_ALLOWED='NO'
