617902bb67f7b48c9c4516e74caf7a11eb1ced52
Philip Hofstetter <phofstetter@sensational.ch>
2025-10-13 16:02:01 +0200

    try to be less brittle to find simulator destination

--- run-tests.sh
+++ run-tests.sh
@@ -8,14 +8,14 @@ fi
 
 mkdir -p result
 
-xcodebuild test -showdestinations -scheme "Daylight Watch App" -target "Daylight Watch AppTests"
-
-id="$(xcodebuild test -showdestinations -scheme "Daylight Watch App" -target "Daylight Watch AppTests" | grep "watchOS Simulator" | grep "Apple Watch Series 11 (46mm)" | head -n 1 | grep -Eo 'id:.*?,' | sed  's/id://' | sed 's/,$//')"
-echo "Running on destination $id"
+#xcodebuild test -showdestinations -scheme "Daylight Watch App" -target "Daylight Watch AppTests"
+#
+#id="$(xcodebuild test -showdestinations -scheme "Daylight Watch App" -target "Daylight Watch AppTests" | grep "watchOS Simulator" | grep "Apple Watch Series 11 (46mm)" | head -n 1 | grep -Eo 'id:.*?,' | sed  's/id://' | sed 's/,$//')"
+#echo "Running on destination $id"
 
 xcodebuild test \
     -scheme "Daylight Watch App" \
     -target "Daylight Watch AppTests" \
-    -destination id="$id" \
+    -destination 'platform=watchOS Simulator,name=Apple Watch Series 11 (46mm)' \
     -resultBundlePath result/test-results.xcresult \
     CODE_SIGNING_ALLOWED='NO'
